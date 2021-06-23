f07e277701a43c52d08fcdf1215773a09f30f8ea
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-23 22:42:46 +0530

    Initial Commit

---
+++ SwiftUI-Understanding_EnvironmentObject/SwiftUI_Understanding_EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Understanding_EnvironmentObjectApp.swift
+//  SwiftUI-Understanding_EnvironmentObject
+//
+//  Created by Arpit Dixit on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Understanding_EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
