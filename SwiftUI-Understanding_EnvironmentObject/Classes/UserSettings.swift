14cfe4654b320e28e777e5e85c71597adcfee4fa
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-23 22:56:49 +0530

    first commit

---
+++ SwiftUI-Understanding_EnvironmentObject/Classes/UserSettings.swift
@@ -0,0 +1,8 @@
+//
+//  UserSettings.swift
+//  SwiftUI-Understanding_EnvironmentObject
+//
+//  Created by Arpit Dixit on 23/06/21.
+//
+
+import Foundation
---
+++ SwiftUI-Understanding_EnvironmentObject/Views/FancyScoreView.swift
@@ -0,0 +1,20 @@
+//
+//  FancyScoreView.swift
+//  SwiftUI-Understanding_EnvironmentObject
+//
+//  Created by Arpit Dixit on 23/06/21.
+//
+
+import SwiftUI
+
+struct FancyScoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FancyScoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        FancyScoreView()
+    }
+}
